67a2ef36a92080dabc7a02f89829994ef03dab18
Michael Stal <michael.stal@gmail.com>
2022-11-01 16:46:53 +0100

    removed demo code from class

--- dialogue.py
+++ dialogue.py
@@ -71,24 +71,4 @@ class Dialogue:
             # print acknowledgement for answering the question
             print(self.ack)
         return value
- 
-def demo():               
-    interaction = Dialogue( title = "Question of the day", 
-                            prompt = "% ", 
-                            question = "How old are you?", 
-                            hints = "(you should lie)", 
-                            ack = "Thanks for your answer",  
-                            predicate = lambda x: int(x) in range(0, 100), 
-                            conversion = lambda x: int(x)
-                         )
-                            
-    interaction.setRepetitions(2)
-    interaction.setLineCharacter(" ")
-    answer = interaction.exec()
-    if answer == None:
-        print("I see, you don't wanna answer this question")
-    else:
-        print("You are " + str(answer))
     
-
-        
